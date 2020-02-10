ed6b88f33db71071208def2b1f15542db83697ae
legonaught <57459145+legonaught@users.noreply.github.com>
2020-02-09 21:21:58 -0500

    85
    Added private field with accessor

--- Book.java
+++ Book.java
@@ -11,6 +11,7 @@ class Book
      // The fields.
     private String author;
     private String title;
+    private int pages;
 
     /**
      * Set the author and title fields when this object
@@ -20,6 +21,7 @@ class Book
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = bookPages;
     }
 
     public String getAuthor()
@@ -30,6 +32,11 @@ class Book
     public String getTitle()
     {
         return title;
+    }
+    
+    public int getPages()
+    {
+        return pages;
     }
     
     public void printAuthor()
